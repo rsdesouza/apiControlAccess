02fdcf9ce66a1da45970b9742323b1721fc73253
rsdesouza <sdesouza.rodolfo@gmail.com>
2021-07-08 14:50:48 -0300

    project controlaccess created

---
+++ controlaccess/src/main/java/com/dio/controlaccess/ControlaccessApplication.java
@@ -0,0 +1,13 @@
+package com.dio.controlaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlaccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlaccessApplication.class, args);
+	}
+
+}
